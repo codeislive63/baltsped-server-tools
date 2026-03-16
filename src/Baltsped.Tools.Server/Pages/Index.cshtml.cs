287ad2f46a40bff9ee2a7348c5d84e1f5fa6767a
codeislive63 <codeislive63@gmail.com>
2026-03-16 15:05:46 +0300

    Добавьте файлы проекта.

---
+++ src/Baltsped.Tools.Server/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Baltsped.Tools.Server.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ tests/Baltsped.Tools.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Baltsped.Tools.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
