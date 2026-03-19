377c0472e66db152fed27f845999f357cfa509c0
codeislive63 <codeislive63@gmail.com>
2026-03-19 10:10:20 +0300

    chore(hosting): remove kestrel https endpoint configuration

--- src/Baltsped.Tools.Server/Program.cs
+++ src/Baltsped.Tools.Server/Program.cs
@@ -1,7 +1,6 @@
 using Baltsped.Tools.Server.Database.Extensions;
 using Baltsped.Tools.Server.Features.DmReplace;
 using Baltsped.Tools.Server.Logging;
-using Microsoft.AspNetCore.HttpOverrides;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,28 +25,11 @@ builder.Services.AddRazorPages();
 builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddScoped<IDmReplaceService, DmReplaceService>();
 
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-});
-
 var app = builder.Build();
-
-app.UseForwardedHeaders();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-if (IsHttpsConfigured(builder.Configuration))
-{
-    app.UseHttpsRedirection();
-}
-else
-{
-    app.Logger.LogWarning("HTTPS redirection is disabled because HTTPS endpoint is not configured");
 }
 
 app.UseStaticFiles();
@@ -57,30 +39,3 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
-
-// Проверяет, настроен ли HTTPS endpoint для безопасного редиректа
-static bool IsHttpsConfigured(IConfiguration configuration)
-{
-    var explicitHttpsConfig = configuration["HTTPS_PORT"]
-                              ?? configuration["ASPNETCORE_HTTPS_PORT"]
-                              ?? configuration["Kestrel:Endpoints:Https:Url"];
-
-    if (!string.IsNullOrWhiteSpace(explicitHttpsConfig))
-    {
-        return true;
-    }
-
-    var rawUrls = configuration["ASPNETCORE_URLS"] ?? configuration["urls"];
-
-    if (string.IsNullOrWhiteSpace(rawUrls))
-    {
-        return false;
-    }
-
-    var urls = rawUrls.Split(';', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-
-    return urls.Any(url =>
-        Uri.TryCreate(url, UriKind.Absolute, out var uri)
-        && uri.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase)
-    );
-}
